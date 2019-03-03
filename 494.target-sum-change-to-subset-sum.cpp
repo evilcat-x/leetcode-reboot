783c6e42ed2735bcc969b192013625e3d7e64471
Sen Li <seli@e1z1r7p16.42.us.org>
2019-03-02 21:15:22 -0800

    interesting convertion to other question

--- 494.target-sum-change-to-subset-sum.cpp
+++ 494.target-sum-change-to-subset-sum.cpp
@@ -49,20 +49,23 @@
 #include <numeric>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findTargetSumWays(vector<int>& nums, int S) {
-		int sum = accumulate(nums.begin(),nums.end(),0);
-		return sum < S || (S + sum) & 1 != 0 ? 0 : subsetSum(nums, (S+sum) >> 1);
-    }
-    int subsetSum(vector<int>& nums, int S) {
-		vector<int> dp(S+1, 0);
+	int findTargetSumWays(vector<int> &nums, int S)
+	{
+		int sum = accumulate(nums.begin(), nums.end(), 0);
+		return sum < S || (S + sum) & 1 != 0 ? 0 : subsetSum(nums, (S + sum) >> 1);
+	}
+	int subsetSum(vector<int> &nums, int S)
+	{
+		vector<int> dp(S + 1, 0);
 		dp[0] = 1;
-		for (auto& n : nums) {
+		for (auto &n : nums)
+		{
 			for (int i = S; i >= n; i--)
-				dp[i] += dp[i-n];
+				dp[i] += dp[i - n];
 		}
 		return dp[S];
-    }
+	}
 };
-
